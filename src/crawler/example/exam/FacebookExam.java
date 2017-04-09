53eef48ad2123b89fbc76305408aa4f9ecbfc678
samuel81718 <samuel81718@gmail.com>
2017-04-09 11:18:53 +0800

    ＦＢ作業

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -24,9 +24,9 @@ public class FacebookExam {
 		// 遠端資料路徑
 
 		String uri = 
-				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				"https://graph.facebook.com/v2.6/"
+				+ "crazyck101/feed?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBAHqAlbzsP63xTLClKAP4JRpgOYL3WY6kgru3WaFMcfOmV5ljvphLTyFDYVr5KNlTZBumTEicevlmrQNvnRYpNfGcVRHBj6Mm24LNY58pZBhZCOWX3CDNAb3zZBX48xYuTDSYkidQB1SBnRY7GjZB0VSl9yjBKjn6CmvXMQTo7NI29QXNyW2EZD";
 
 
 		Elements elems =
@@ -41,7 +41,7 @@ public class FacebookExam {
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reaction").text();
 
 
 			output += id + "," + reactions + "\n";
